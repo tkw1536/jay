a68d1ad0d61ebfa6fbd1ae72e3705bc96256fb7b
bklw <x32000@gmail.com>
2015-10-16 17:51:36 +0200

    Add user models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from settings.models import VotingSystem
 
 # Create your models here.
+class Admin(models.Model):
+	user = models.ForeignKey(User)
+	system = models.ForeignKey(VotingSystem)
+
+class SuperAdmin(models.Model):
+	user = models.ForeignKey(User)
+
+class UserProfile(models.Model):
+	username = models.CharField(max_length = 32, unique = True)
+	fullname = models.CharField(max_length = 128)
+	eid = models.IntegerField()
