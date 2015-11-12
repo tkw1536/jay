5b6de7e8f79182050eccaf0dc14bca80e67fcb03
bklw <x32000@gmail.com>
2015-11-12 10:39:51 +0100

    Extend auth backend to ingest users realname and email

--- users/ojub_auth.py
+++ users/ojub_auth.py
@@ -12,23 +12,42 @@ class OjubBackend(object):
 	The URL for the server is configured by OPENJUB_BASE in the settings.
 
 	This class does not fill in user profiles, this has to be handled
-	in other 
+	in other places
 	"""
 	def authenticate(self, username=None, password=None):
-		r = requests.post(OPENJUB_BASE + "auth/signin", 
+		r = requests.post(OPENJUB_BASE + "auth/signin",
 			data = {'username':username, 'password': password})
 
 		if r.status_code != requests.codes.ok:
 			return None
 
-		try:
-			user = User.objects.get(username=username)
-		except User.DoesNotExist:
-			user = User(username=username, password="this does not matter")
+		resp = r.json()
 
+		uname = resp['user']
+		token = resp['token']
+
+		try:
+			user = User.objects.get(username=uname)
+		except User.DoesNotExist:
+			user = User(username=uname, password="stored in LDAP")
+
+			# TODO Don't hardcode this
 			if user.username in ["lkuboschek", "twiesing"]:
 				user.is_staff = True
 				user.is_superuser = True
+
+			details = requests.get(OPENJUB_BASE + "user/me",
+				params = {'token':token})
+
+			if details.status_code != requests.codes.ok:
+				print("Could not get user details")
+				return None
+
+			data = details.json()
+
+			user.first_name = data['firstName']
+			user.last_name = data['lastName']
+			user.email = data['email']
 
 			user.save()
 
@@ -39,4 +58,3 @@ class OjubBackend(object):
 			return User.objects.get(pk=user_id)
 		except User.DoesNotExist:
 			return None
-
