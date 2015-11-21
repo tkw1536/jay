a16933976f35da93fa1b9f129a09b65c169e6326
Leonhard <x32000@gmail.com>
2015-11-21 01:42:18 +0100

    Add demo view to showcase vote render

--- jay/demo_urls.py
+++ jay/demo_urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url
-from django.contrib import admin
 
 from django.views.generic import TemplateView
 
+from . import demo_views
+
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="base/base.html")),
-    url(r'^vote$', TemplateView.as_view(template_name="vote/vote_vote.html")),
+    url(r'^vote/(?P<vote_name>[\w-]+)$', demo_views.vote),
 ]
