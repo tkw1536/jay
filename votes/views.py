ed63fb03b81be9eeff21762674290eb1ff69b452
Leonhard <x32000@gmail.com>
2015-11-23 12:45:36 +0100

    Add basic vote processing. Missing status checks at the moment

--- votes/views.py
+++ votes/views.py
@@ -8,7 +8,7 @@ from django.views.generic import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
-from votes.models import Vote
+from votes.models import Vote, Option, Status, ActiveVote
 from filters.models import UserFilter
 from users.models import UserProfile
 
@@ -28,27 +28,84 @@ class VoteView(View):
         options = vote.option_set.all()
         ctx['options'] = options
 
+        # TODO Check status of vote
+
         try:
             user_details = json.loads(request.user.profile.details)
 
             if not filter:
                 ctx['alert_head'] = "No filter given."
                 ctx['alert_text'] = "This vote has not been configured properly."
-                error = True
             elif not filter.matches(user_details):
                 ctx['alert_head'] = "Not eligible"
                 ctx['alert_text'] = "You are not eligible for this vote. Tough luck."
-                error = True
 
         except UserProfile.DoesNotExist:
             ctx['alert_head'] = "User details invalid."
             ctx['alert_text'] = "Your user details could not be retrieved from CampusNet. Please log out and try again later."
+
+        if 'alert_head' in ctx:
             error = True
 
         if not error:
-            return render_to_response("vote/vote_vote.html", context=ctx)
+            return render(request, "vote/vote_vote.html", context=ctx)
         else:
-            return render_to_response("vote/vote_error.html", context=ctx)
+            return render(request, "vote/vote_error.html", context=ctx, status=403)
 
-    def post(self, request):
-        return HttpResponse("TODO: Write vote processing code")
+    def render_error_response(self, ctx):
+        return render_to_response("vote/vote_error.html", context=ctx)
+
+    @method_decorator(login_required)
+    def post(self, request, system_name, vote_name):
+        ctx = {}
+
+        # Make sure all the POST params are present
+        if not 'vote_id' in request.POST:
+            ctx['alert_head'] = "Something happened."
+            ctx['alert_text'] = "Go back to the start and try again."
+            return render_error_response(ctx)
+
+        if not 'options_selected' in request.POST:
+            ctx['alert_head'] = "Something happened."
+            ctx['alert_text'] = "Go back and start over."
+            return render_error_response(ctx)
+
+
+        options = json.loads(request.POST['options_selected'])
+
+        vote = get_object_or_404(Vote, machine_name=vote_name)
+        filter = vote.filter
+
+        error = False
+
+        options_obj = Option.objects.filter(id__in=options, vote__id=vote.id)
+
+        ctx['vote'] = vote
+        ctx['options'] = options_obj
+
+        if not (len(options_obj) >= vote.min_votes and len(options_obj) <= vote.max_votes):
+            ctx['alert_head'] = "Invalid selection."
+            ctx['alert_text'] = "Invalid number of options selected."
+            return self.render_error_response(ctx)
+
+        try:
+            user_details = json.loads(request.user.profile.details)
+
+            if not filter:
+                ctx['alert_head'] = "No filter given."
+                ctx['alert_text'] = "This vote has not been configured properly."
+            elif not filter.matches(user_details):
+                ctx['alert_head'] = "Not eligible"
+                ctx['alert_text'] = "You are not eligible for this vote. Tough luck."
+
+        except UserProfile.DoesNotExist:
+            ctx['alert_head'] = "User details invalid."
+            ctx['alert_text'] = "Your user details could not be retrieved from CampusNet. Please log out and try again later."
+
+        if 'alert_head' in ctx:
+            return self.render_error_response(ctx)
+
+        # TODO Check status of vote before counting vote
+        # TODO Do the actual vote counting
+
+        return HttpResponse("It worked, but did nothing. TODO Put something more affirmative here.")
