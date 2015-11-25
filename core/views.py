f9414b98a0bad6c5162334d9714c87ec06031100
bklw <x32000@gmail.com>
2015-11-25 16:45:17 +0100

    Add infos to home screen

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ import json
 
 from django.shortcuts import render
 
-from votes.models import Vote
+from votes.models import Vote, Status
 from filters.models import UserFilter
 from users.models import UserProfile
 
@@ -10,12 +10,20 @@ from users.models import UserProfile
 def home(request):
     ctx = {}
     votes = Vote.objects.all()
+    results = Vote.objects.filter(status__stage=Status.PUBLIC)
 
     if request.user.is_authenticated():
         details = json.loads(request.user.profile.details)
-        votes = filter(lambda x: x.filter.matches(details), votes)
+        votes_shown = [v for v in votes if v.filter.matches(details)]
+
         ctx["vote_list_title"] = "Your votes"
 
-    ctx["votes"] = votes
+        if len(votes) != len(votes_shown):
+            ctx["eligible_alert"] = True
+    else:
+        votes_shown = votes
+
+    ctx["votes"] = votes_shown
+    ctx["results"] = results
 
     return render(request, "home.html", ctx)
