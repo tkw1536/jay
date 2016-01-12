3b8813f909d23ab285a19a4dee10396da0a2744c
Tom Wiesing <tkw01536@gmail.com>
2016-01-12 12:23:59 +0100

    Increase timeout for http

--- users/ojub_auth.py
+++ users/ojub_auth.py
@@ -83,24 +83,24 @@ def get_all(username, password):
 
 	uname = resp['user']
 	token = resp['token']
-	
+
 	users = []
-	
-	TIMEOUT = 10
-	
+
+	TIMEOUT = 60
+
 	request = requests.get(OPENJUB_BASE + "query",
-		params = {'token':token, 'limit': 1000}, timeout = TIMEOUT)
-		
+		params = {'token':token, 'limit': 20000}, timeout = TIMEOUT)
+
 	while True:
 		if request.status_code != requests.codes.ok:
 			return None
 		else:
 			# read json
 			resjson = request.json()
-			
+
 			# load all the users
 			users += resjson["data"]
-			
+
 			# if there was no data or no next field, continue
 			if len(resjson["data"]) == 0 or not resjson["next"]:
 				return users
